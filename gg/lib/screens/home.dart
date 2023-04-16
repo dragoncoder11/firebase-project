e4a7eaed6688b6f9998efcb5da55cd2bef5ae4c2
dragoncoder11 <mostafaamr9085@gmail.com>
2023-04-16 17:39:43 +0200

    cc

--- gg/lib/screens/home.dart
+++ gg/lib/screens/home.dart
@@ -2,31 +2,58 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:gg/screens/ioginscreen.dart';
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  var numindex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(234, 255, 255, 255),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('home'),
-            MaterialButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-                Navigator.of(context)
-                    .pushReplacement(MaterialPageRoute(builder: (_) {
-                  return LoginScreen();
-                }));
-              },
-              color: Colors.blueGrey,
-            )
-          ],
-        ),
-      ),
-    );
+      
+        bottomNavigationBar: BottomNavigationBar(
+          
+          iconSize: 32,
+          selectedFontSize: 14,
+          selectedItemColor: Colors.white,
+          selectedLabelStyle: TextStyle(color: Colors.white),
+          unselectedItemColor: Colors.grey,
+          unselectedLabelStyle:TextStyle(color: Colors.grey), 
+
+          elevation: 0,
+          backgroundColor: Colors.purple,
+            currentIndex: numindex,
+            onTap: (value) {
+              setState(() {
+                numindex = value;
+              });
+            },
+            items: [
+              BottomNavigationBarItem(icon: Icon(Icons.home,), label: 'Home',),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.currency_bitcoin,), label: 'Services'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.location_city_sharp,), label: 'Activities'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.person,), label: 'Profile'),
+            ]),
+        backgroundColor: Color.fromARGB(234, 255, 255, 255),
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(color: Color.fromARGB(237, 255, 255, 255)),
+          child: Column(
+            children: [
+              Center(
+                child: Text('data'),
+              )
+            ],
+          ),
+        ));
   }
 }
--- gg/lib/screens/signupscreen.dart
+++ gg/lib/screens/signupscreen.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:gg/screens/auth.dart';
 import 'package:gg/screens/home.dart';
 import 'package:gg/screens/ioginscreen.dart';
 import 'package:gg/widgets/button.dart';
