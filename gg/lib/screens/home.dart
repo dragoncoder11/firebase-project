39fc1ede9cc7c5ea8d934a5d7b41f2093b9bf537
dragoncoder11 <mostafaamr9085@gmail.com>
2023-04-17 18:06:09 +0200

    commit

--- gg/lib/screens/home.dart
+++ gg/lib/screens/home.dart
@@ -1,6 +1,7 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:gg/pages/home_screen.dart';
 import 'package:gg/pages/profile_screen.dart';
 import 'package:gg/screens/ioginscreen.dart';
 
@@ -14,7 +15,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   var numindex = 0;
   var l=[
-    Text('data'),
+HomeScreen(),
     Text('data'),
     Text('data'),
     ProfileScreen(),
